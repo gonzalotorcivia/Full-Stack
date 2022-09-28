b47cb41158ac7a586f52a94e3e75343ace5f89b7
Torcivia Gonzalo <gonzalotorcivia@gmail.com>
2022-09-28 20:58:56 -0300

    *

--- Guia 7/Ejercicios/Guia7-Ej8/src/guia7/ej8/Guia7Ej8.java
+++ Guia 7/Ejercicios/Guia7-Ej8/src/guia7/ej8/Guia7Ej8.java
@@ -14,27 +14,40 @@ public class Guia7Ej8 {
        
           Scanner leer = new Scanner(System.in).useDelimiter("\n").useLocale(Locale.US);
           // creamos el objeto
-          Cadena frasepalabra = new Cadena();
+          Cadena frase1 = new Cadena();
           
           System.out.println("Ingrese una frase o palabra");
           
           String fraseingresada = leer.nextLine();
           
-          frasepalabra.setFrase(fraseingresada);//guardo la frase atraves del set
-          frasepalabra.setLongitudfrase(fraseingresada.length()); //guardo la longitud atraves del set
+          frase1.setFrase(fraseingresada);//guardo la frase atraves del set
+          frase1.setLongitudfrase(fraseingresada.length()); //guardo la longitud atraves del set
           
           ServicioCadena sc = new ServicioCadena();
           
           //traigo del servicio los metodos
-          System.out.println("La frase o palabra ingresada tiene " +sc.mostrarVocales(frasepalabra)+ " vocales");
+          System.out.println("La frase o palabra ingresada tiene " +sc.mostrarVocales(frase1)+ " vocales");
          
-          sc.invertirFrase(frasepalabra);
+          sc.invertirFrase(frase1);
           
-          sc.vecesRepetido(fraseingresada);
+          sc.vecesRepetidos(frase1);
+          
+          System.out.println("Ingrese una nueva frase o palabra");
+          String frase2 = leer.next();
+          sc.compararLonguitud(frase1,frase2);
+          
+          System.out.println("Ingrese una nueva frase");
+          String frase3 = leer.next();
+          sc.unirfrase(frase1, frase3);
           
           
+          System.out.println("Ingrese un Caracter");
+          String caracterA = leer.next();
+          sc.remplazar(frase1, caracterA);
           
-          
+          System.out.println("Ingrese la letra a verificar");
+          String letra = leer.next();
+          sc.contiene(frase1, letra);
           
     }
     
--- Guia 7/Ejercicios/Guia7-Ej8/src/servicio/ServicioCadena.java
+++ Guia 7/Ejercicios/Guia7-Ej8/src/servicio/ServicioCadena.java
@@ -1,13 +1,17 @@
 package servicio;
 
 import entidades.Cadena;
+import java.util.Scanner;
 
 public class ServicioCadena {
 
+    public Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    int contador = 0;
+
     public int mostrarVocales(Cadena f1) {
 
-      String frase=f1.getFrase();
-        
+        String frase = f1.getFrase();
+
         int contadorVocales;
 
         contadorVocales = 0;
@@ -16,64 +20,103 @@ public class ServicioCadena {
 
             if (("a").equalsIgnoreCase(frase.substring(i, i + 1)) || ("e").equalsIgnoreCase(frase.substring(i, i + 1)) || ("i").equalsIgnoreCase(frase.substring(i, i + 1))
                     || ("o").equalsIgnoreCase(frase.substring(i, i + 1)) || ("u").equalsIgnoreCase(frase.substring(i, i + 1))) {
-           
+
                 contadorVocales++;
-                
+
             }
 
-            
         }
-        
+
         return contadorVocales;
     }
-    
+
     public void invertirFrase(Cadena fra) {
-    
-        String fraseoriginal=fra.getFrase();
+
+        String fraseoriginal = fra.getFrase();
         String fraseinvertida = "";
         // Recorrer la frase original de atras para adelante
-        for (int i = fraseoriginal.length()-1; i >= 0; i--) {
-            
+        for (int i = fraseoriginal.length() - 1; i >= 0; i--) {
+
             fraseinvertida += fraseoriginal.charAt(i);
-            
+
         }
-    
-        System.out.println("Frase Original es: " +fraseoriginal);
-        System.out.println("Frase Invertida es: " +fraseinvertida);
+
+        System.out.println("Frase Original es: " + fraseoriginal);
+        System.out.println("Frase Invertida es: " + fraseinvertida);
     }
-    
-    public void vecesRepetido(String letra) {
-    
-        //System.out.println("Ingrese el caracter que desea contar");
-                    
-        int contadorletra = 0;
-        
-        // for (int i = 0; i < contenidoCadena.length(); i++)
-        
-        for (int i = 0; i < frase.length(); i++) {
-            
-           if ((letra).equalsIgnoreCase(frase.substring(i,i+1)) ) {
-                
-               contadorletra++;
+
+    public int vecesRepetidos(Cadena caracter1) {
+
+        String texto = caracter1.getFrase();
+
+        contador = 0;
+
+        System.out.println("Ingrese un caracter que desee buscar: ");
+
+        String letra = leer.next();
+
+        for (int i = 0; i < caracter1.getLongitudfrase(); i++) {
+
+            if (texto.substring(i, i + 1).equalsIgnoreCase(letra)) {
+
+                contador++;
             }
         }
-        
-        System.out.println("la letra: "+letra+" aparece "+ frase +" veces.");
-        
-        }
-        
-        
-    
-        
-        
+
+        System.out.println("El caracter " + letra + " se repite " + contador + " veces");
+
+        return contador;
     }
-    
-    
-    
-    
-    
+
+    public void compararLonguitud(Cadena fraseoriginal, String frasenueva) {
+
+        if (fraseoriginal.getLongitudfrase() > frasenueva.length()) {
+            System.out.println("La primera frase es mas larga que la segunda");
+        } else if (fraseoriginal.getLongitudfrase() < frasenueva.length()) {
+            System.out.println("La segunda frase es mas larga que la primera");
+        } else {
+            System.out.println("Las frases tienen el mismo tamaño");
+        }
+    }
+
+    public void unirfrase(Cadena fraseoriginal, String frasenueva2) {
+
+        System.out.println("Las frases unidas quedarian " + fraseoriginal.getFrase().concat(" " + frasenueva2));
+
+    }
+
+    public void remplazar(Cadena fraseoriginal, String caracterA) {
+
+        System.out.println("La frase con la letra reemplazada seria:");
+
+        for (int i = 0; i < fraseoriginal.getLongitudfrase(); i++) {
+
+            if (fraseoriginal.getFrase().substring(i, i + 1).equalsIgnoreCase("A")) {
+
+                System.out.print(caracterA);
+
+            } else {
+
+                System.out.print(fraseoriginal.getFrase().substring(i, i + 1));
+            }
+
+            System.out.println("");
+        }
+    }
+
+    public boolean contiene(Cadena fraseoriginal, String letra) {
+
+        boolean aux = false;
+
+        for (int i = 0; i < fraseoriginal.getLongitudfrase(); i++) {
+
+            if (fraseoriginal.getFrase().substring(i, i + 1).equalsIgnoreCase(letra)) {
+                aux = true;
+            }
+
+        }
+        System.out.println("La letra ingresada es " +aux);
+        return aux;
+    }
+
 }
-
-
-    
-
